cd752d37e16c9d4082134613f99e301e6849023a
Mr-Dark65 <steevenloor2412@gmail.com>
2025-11-15 21:33:11 -0500

    Se realizo un gestor para el modelo de usuarios.

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils import timezone
+import os
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """
+    Gestor personalizado para el modelo de usuario.
+    """
+    def create_user(self, email, username, password=None, **extra_fields):
+        """
+        Crea y guarda un usuario regular.
+        """
+        if not email:
+            raise ValueError('El usuario debe tener un email')
+        if not username:
+            raise ValueError('El usuario debe tener un username')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        """
+        Crea y guarda un superusuario.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser debe tener is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser debe tener is_superuser=True.')
+        
+        return self.create_user(email, username, password, **extra_fields)
